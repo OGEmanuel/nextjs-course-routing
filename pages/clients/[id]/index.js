385a1cf56450da48e784ebdf1bec4b13da7ccc89
OGEmanuel <emanuolumide@mail.com>
2023-03-03 14:30:00 +0100

    more on routing

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -4,9 +4,19 @@ const ClientProjectsPage = () => {
   const router = useRouter();
 
   console.log(router.query);
+
+  const loadProjectHandler = () => {
+    // Load data...
+    router.push({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: { id: 'max', clientprojectid: 'projecta' },
+    });
+  };
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 };
